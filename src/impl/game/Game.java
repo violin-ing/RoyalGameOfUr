e5b39480cd156abf1f56b92715b66a48201fe880
ElusiveI <cole.chappell@protonmail.ch>
2024-04-02 14:42:49 +0100

    comment

--- src/impl/game/Game.java
+++ src/impl/game/Game.java
@@ -149,14 +149,18 @@ public class Game {
     }
 
     public void getFuturePositions(String player, int roll, List<int[]> currentMovablePositions) {
-        
+        // position of winning tile, 6 on a player strip
     }
 
     // will return -1 if this chip cannot be moved, otherwise will return the postion and strip it will be moved to.
     private boolean isMoveable(String player, int roll, int movePosition, int strip) {
         // validiation of moves happens, here, so the above method can be simplified more easily.
 
-        // find when chip is not movable, 
+        // find when chip is not movable:
+        // check if chip is moving onto a rosette tile
+        // check the tile its moving to by using: currentBoard, the strip its moving to (p1, p2, middle)
+        // if this rosette tile is unoccupied or has current player chip on it can move here.
+        // otherwise, this is not a valid move.
         return false;
     }
 
