da62ab9b17f7d9c586b25b00dbca60bedc29e722
Eugene Kong <ek242@st-andrews.ac.uk>
2024-04-04 21:00:32 +0100

    ok

--- src/impl/networking/GameSession.java
+++ src/impl/networking/GameSession.java
@@ -22,9 +22,6 @@ public class GameSession {
      private String p1Address;
      private String p2Address;
 
-     PrintWriter p1Out, p2Out;
-     BufferedReader p1In, p2In;
-
      private volatile ConcurrentHashMap<String, Long> lastHeartbeatTime = new ConcurrentHashMap<>();
 
      /**
@@ -43,15 +40,6 @@ public class GameSession {
 
           lastHeartbeatTime.put(p1Address, System.currentTimeMillis());
           lastHeartbeatTime.put(p2Address, System.currentTimeMillis());
-
-          try {
-               this.p1Out = new PrintWriter(player1.getOutputStream(), true);
-               this.p1In = new BufferedReader(new InputStreamReader(player1.getInputStream()));
-               this.p2Out = new PrintWriter(player2.getOutputStream(), true);
-               this.p2In = new BufferedReader(new InputStreamReader(player2.getInputStream()));
-          } catch (IOException e) {
-               e.printStackTrace();
-          }
 
           Thread heartbeatListener = new Thread(() -> {
                try (DatagramSocket socket = new DatagramSocket(null)) {
@@ -90,92 +78,86 @@ public class GameSession {
       * @param playerSocket The socket of the player to handle.
       * @param playerLabel A label identifying the player (e.g., "Player 1").
       */
-     private void handlePlayers(Socket p1Socket, Socket p2Socket) throws IOException, InterruptedException {
-          playGame(p1Out, p1In, p2Out, p2In);
-     }
-
-     /**
-      * Facilitates the gameplay between the two players, alternating turns, and processing player inputs.
-      * This method also monitors for player disconnection and timeouts, updating game state accordingly.
-      * 
-      * @param p1Out PrintWriter for player 1 output.
-      * @param p1In BufferedReader for player 1 input.
-      * @param p2Out PrintWriter for player 2 output.
-      * @param p2In BufferedReader for player 2 input.
-      * @throws IOException If an I/O error occurs during communication with the players.
-      */
-     private void playGame(PrintWriter p1Out, BufferedReader p1In, PrintWriter p2Out, BufferedReader p2In) throws IOException, InterruptedException {
+     private void handlePlayers(Socket p1Socket, Socket p2Socket) {
           // Send signal for client to know if they start first or wait for their turn first
-          p1Out.println("startfirst");
-          p2Out.println("waitfirst");
+          try (PrintWriter p1Out = new PrintWriter(p1Socket.getOutputStream(), true);
+               BufferedReader p1In = new BufferedReader(new InputStreamReader(p1Socket.getInputStream()));
+               PrintWriter p2Out = new PrintWriter(p2Socket.getOutputStream(), true);
+               BufferedReader p2In = new BufferedReader(new InputStreamReader(p2Socket.getInputStream())); ) {
 
-          System.out.println("Server: Player turns assigned.");
-
-          // Schedule a task to check for timeouts
-          ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
-          executorService.scheduleAtFixedRate(() -> {
-               long currentTime = System.currentTimeMillis();
-               lastHeartbeatTime.forEach((ipAddr, lastHeartbeatTime) -> {
-                    if ((currentTime - lastHeartbeatTime) > TIMEOUT) {
-                         if (ipAddr.equals(p1Address)) {
-                              System.out.println("Server: Player 1 (" + ipAddr + ") has disconnected due to timeout.");
-                              try {
-                                   p2Out.print("opponentdc\r\n");
-                                   System.out.println("Server: Player 2 (" + p2Address + ") socket closed.");
-                                   player2.close();
-                                   throw new InterruptedException();
-                               } catch (Exception e) {
-                                   e.printStackTrace();
-                               }                          
-                         } else {
-                              System.out.println("Server: Player 2 (" + ipAddr + ") has disconnected due to timeout.");
-                              try {
-                                   p1Out.println("opponentdc");
-                                   System.out.println("Server: Player 1 (" + p1Address + ") socket closed.");
-                                   player1.close();
-                                   throw new InterruptedException();
-                               } catch (Exception e) {
-                                   e.printStackTrace();
-                               }     
+               p1Out.println("startfirst");
+               p2Out.println("waitfirst");
+     
+               System.out.println("Server: Player turns assigned.");
+     
+               // Schedule a task to check for timeouts
+               ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+               executorService.scheduleAtFixedRate(() -> {
+                    long currentTime = System.currentTimeMillis();
+                    lastHeartbeatTime.forEach((ipAddr, lastHeartbeatTime) -> {
+                         if ((currentTime - lastHeartbeatTime) > TIMEOUT) {
+                              if (ipAddr.equals(p1Address)) {
+                                   System.out.println("Server: Player 1 (" + ipAddr + ") has disconnected due to timeout.");
+                                   try {
+                                        p2Out.println("opponentdc");
+                                        System.out.println("Server: Player 2 (" + p2Address + ") socket closed.");
+                                        player2.close();
+                                        throw new InterruptedException();
+                                        } catch (Exception e) {
+                                        e.printStackTrace();
+                                        }                          
+                              } else {
+                                   System.out.println("Server: Player 2 (" + ipAddr + ") has disconnected due to timeout.");
+                                   try {
+                                        p1Out.println("opponentdc");
+                                        System.out.println("Server: Player 1 (" + p1Address + ") socket closed.");
+                                        player1.close();
+                                        throw new InterruptedException();
+                                        } catch (Exception e) {
+                                        e.printStackTrace();
+                                        }     
+                              }
+                              executorService.shutdown();
                          }
-                         executorService.shutdown();
+                    });
+               }, 0, 1, TimeUnit.SECONDS); // Check for heartbeat every second
+     
+               Thread p1Listener = new Thread(() -> {
+                    while (true) {
+                         try {
+                              String p1info = p1In.readLine();
+                              if (p1info != null) {
+                                   p2Out.println(p1info);
+                              }
+                         } catch (Exception e) {
+                              e.printStackTrace();
+                              break;
+                         }
                     }
                });
-          }, 0, 1, TimeUnit.SECONDS); // Check for heartbeat every second
-
-          // In this method, we need to pass in the array(s) that correspond to the game GUI
-          // On the client side, this will directly update the GUI displayed on their screen
-          // Server updates the GUI on both clients' machines after every move (roll + chip movement)
-
-          Thread p1Listener = new Thread(() -> {
-               while (true) {
-                    try {
-                         String p1info = p1In.readLine();
-                         if (p1info != null) {
-                              p2Out.println(p1info);
+     
+               Thread p2Listener = new Thread(() -> {
+                    while (true) {
+                         try {
+                              String p2info = p2In.readLine();
+                              if (p2info != null) {
+                                   p1Out.println(p2info);
+                              }
+                         } catch (Exception e) {
+                              e.printStackTrace();
+                              break;
                          }
-                    } catch (Exception e) {
-                         e.printStackTrace();
-                         break;
                     }
-               }
-          });
+               });
+     
+               p1Listener.start();
+               p2Listener.start();
 
-          Thread p2Listener = new Thread(() -> {
                while (true) {
-                    try {
-                         String p2info = p2In.readLine();
-                         if (p2info != null) {
-                              p1Out.println(p2info);
-                         }
-                    } catch (Exception e) {
-                         e.printStackTrace();
-                         break;
-                    }
+                    // Run game
                }
-          });
-
-          p1Listener.start();
-          p2Listener.start();
+          } catch (Exception e) {
+               
+          } 
      }
 }
