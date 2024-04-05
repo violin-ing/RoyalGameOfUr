81d12a087587d198d813646390de593e6964e7d5
ElusiveI <cole.chappell@protonmail.ch>
2024-04-05 15:43:32 +0100

    clonable board

--- src/impl/game/Ai.java
+++ src/impl/game/Ai.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class Ai implements Cloneable{
+public class Ai {
     private Node root;
     public static double[] ROLL_PERCENTAGES = {0.0625, 0.25, 0.375, 0.25, 0.0625};
    //public static int LEVELS = 4;
