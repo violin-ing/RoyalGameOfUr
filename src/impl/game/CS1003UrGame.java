79772ffbf0bf1b88152390c346d80123f41a3a27
Eugene Kong <ek242@st-andrews.ac.uk>
2024-04-05 14:51:30 +0100

    added comments

--- src/impl/game/CS1003UrGame.java
+++ src/impl/game/CS1003UrGame.java
@@ -1,14 +1,7 @@
 import javax.swing.SwingUtilities;
 
 public class CS1003UrGame {
-    // static Counter counter = new Counter();
-    // static Board currentBoard = new Board(counter);
-    // static Dice dice = new Dice();
-
     public static void main(String[] args) {
-        // gameGUI on a new thread?
-        //GameGUI gameGUI = new GameGUI(game);
-        //game.setGameGUI(gameGUI);
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new StartMenuGUI();
