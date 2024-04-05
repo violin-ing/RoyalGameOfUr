ffce8071e864373aefdae521bcfe61b3dff659ce
Eugene Kong <ek242@st-andrews.ac.uk>
2024-04-05 15:23:19 +0100

    added comments

--- src/impl/game/CS1006UrGame.java
+++ src/impl/game/CS1006UrGame.java
@@ -7,8 +7,18 @@ import javax.swing.SwingUtilities;
  * initiates the game by displaying the start menu to the user.
  */
 public class CS1006UrGame {
+    /**
+     * The main method that serves as the entry point of the application. It schedules the creation and
+     * display of the game's start menu on the Swing event dispatch thread to ensure thread-safe operation.
+     * 
+     * @param args Command line arguments passed to the program (not used).
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
+            /**
+             * Runs the code to create and display the game's start menu GUI. This method is executed
+             * on the Event Dispatch Thread (EDT) to ensure compliance with Swing's single-thread model.
+             */
             public void run() {
                 new StartMenuGUI();
             }
