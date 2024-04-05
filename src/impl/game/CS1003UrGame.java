d91aba53ae0b6667d7c70514fb86d712026e43c7
Eugene Kong <ek242@st-andrews.ac.uk>
2024-04-05 15:41:15 +0100

    poo

--- src/impl/game/CS1003UrGame.java
+++ src/impl/game/CS1003UrGame.java
@@ -1,12 +1,12 @@
 import javax.swing.SwingUtilities;
 
 /**
- * The {@code CS1006UrGame} class serves as the entry point for a graphical game application. It uses
+ * The {@code CS1003UrGame} class serves as the entry point for a graphical game application. It uses
  * {@link SwingUtilities#invokeLater} to ensure that the game's user interface is created and updated
  * on the Event Dispatch Thread (EDT), adhering to Swing's single-threaded GUI framework. The main method
  * initiates the game by displaying the start menu to the user.
  */
-public class CS1006UrGame {
+public class CS1003UrGame {
     /**
      * The main method that serves as the entry point of the application. It schedules the creation and
      * display of the game's start menu on the Swing event dispatch thread to ensure thread-safe operation.
